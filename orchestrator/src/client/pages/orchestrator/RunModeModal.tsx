1d4469cc7b484af3d4daf70e3334b0cd1ccd0285
DaKheera47 <shaheergoodboy@gmail.com>
2026-02-07 21:19:42 +0000

    remove badge

--- orchestrator/src/client/pages/orchestrator/RunModeModal.tsx
+++ orchestrator/src/client/pages/orchestrator/RunModeModal.tsx
@@ -1,7 +1,6 @@
 import { ManualImportFlow } from "@client/components/ManualImportFlow";
 import type { AppSettings, JobSource } from "@shared/types";
 import type React from "react";
-import { Badge } from "@/components/ui/badge";
 import { Separator } from "@/components/ui/separator";
 import {
   Sheet,
